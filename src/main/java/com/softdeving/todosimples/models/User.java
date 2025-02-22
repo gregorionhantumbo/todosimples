1eee299f6453ec8d47824a6fa69ecdea37415000
gregorionhantumbo <gregorionhantumbo1@gmail.com>
2025-02-22 07:33:13 +0200

    User created successfully

---
+++ src/main/java/com/softdeving/todosimples/models/User.java
@@ -0,0 +1,67 @@
+package com.softdeving.todosimples.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = User.TABLE_NAME)
+public class User {
+    public interface CreateUser{}
+    public interface UpdateUser{}
+
+    public static final String TABLE_NAME = "user";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    private Long id;
+
+    @Column(name = "username", length = 50, nullable = false, unique = true)
+    @NotBlank(groups = CreateUser.class)
+    private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(name = "password", length = 50, nullable = false)
+    @NotBlank(groups = {CreateUser.class, UpdateUser.class})
+    private String password;
+
+    //private List<Task> tasks new ArrayList<Task>();
+
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
